226a7403c993d0d88bbf6d79eb34685fb7df47d1
ohhheyyyy <ashley.hartt@gmail.com>
2016-05-13 14:38:26 -0400

    Workshop through 2.Build It – Add a Day

--- public/js/main.js
+++ public/js/main.js
@@ -1,63 +1,157 @@
-$(function initializeMap (){
+$(function initializeMap() {
 
-  var graceHopperAcademy = new google.maps.LatLng(40.705086, -74.009151);
+    var graceHopperAcademy = new google.maps.LatLng(40.705086, -74.009151);
 
-  var styleArr = [{
-    featureType: 'landscape',
-    stylers: [{ saturation: -100 }, { lightness: 60 }]
-  }, {
-    featureType: 'road.local',
-    stylers: [{ saturation: -100 }, { lightness: 40 }, { visibility: 'on' }]
-  }, {
-    featureType: 'transit',
-    stylers: [{ saturation: -100 }, { visibility: 'simplified' }]
-  }, {
-    featureType: 'administrative.province',
-    stylers: [{ visibility: 'off' }]
-  }, {
-    featureType: 'water',
-    stylers: [{ visibility: 'on' }, { lightness: 30 }]
-  }, {
-    featureType: 'road.highway',
-    elementType: 'geometry.fill',
-    stylers: [{ color: '#ef8c25' }, { lightness: 40 }]
-  }, {
-    featureType: 'road.highway',
-    elementType: 'geometry.stroke',
-    stylers: [{ visibility: 'off' }]
-  }, {
-    featureType: 'poi.park',
-    elementType: 'geometry.fill',
-    stylers: [{ color: '#b6c54c' }, { lightness: 40 }, { saturation: -40 }]
-  }];
+    var styleArr = [{
+        featureType: 'landscape',
+        stylers: [{ saturation: -100 }, { lightness: 60 }]
+    }, {
+        featureType: 'road.local',
+        stylers: [{ saturation: -100 }, { lightness: 40 }, { visibility: 'on' }]
+    }, {
+        featureType: 'transit',
+        stylers: [{ saturation: -100 }, { visibility: 'simplified' }]
+    }, {
+        featureType: 'administrative.province',
+        stylers: [{ visibility: 'off' }]
+    }, {
+        featureType: 'water',
+        stylers: [{ visibility: 'on' }, { lightness: 30 }]
+    }, {
+        featureType: 'road.highway',
+        elementType: 'geometry.fill',
+        stylers: [{ color: '#ef8c25' }, { lightness: 40 }]
+    }, {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{ visibility: 'off' }]
+    }, {
+        featureType: 'poi.park',
+        elementType: 'geometry.fill',
+        stylers: [{ color: '#b6c54c' }, { lightness: 40 }, { saturation: -40 }]
+    }];
 
-  var mapCanvas = document.getElementById('map-canvas');
+    var mapCanvas = document.getElementById('map-canvas');
 
-  var currentMap = new google.maps.Map(mapCanvas, {
-    center: graceHopperAcademy,
-    zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    styles: styleArr
-  });
-
-  var iconURLs = {
-    hotel: '/images/lodging_0star.png',
-    restaurant: '/images/restaurant.png',
-    activity: '/images/star-3.png'
-  };
-
-  function drawMarker (type, coords) {
-    var latLng = new google.maps.LatLng(coords[0], coords[1]);
-    var iconURL = iconURLs[type];
-    var marker = new google.maps.Marker({
-      icon: iconURL,
-      position: latLng
+    var currentMap = new google.maps.Map(mapCanvas, {
+        center: graceHopperAcademy,
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: styleArr
     });
-    marker.setMap(currentMap);
-  }
 
-  // drawMarker('hotel', [40.705137, -74.007624]);
-  // drawMarker('restaurant', [40.705137, -74.013940]);
-  // drawMarker('activity', [40.716291, -73.995315]);
+    var iconURLs = {
+        hotel: '/images/lodging_0star.png',
+        restaurant: '/images/restaurant.png',
+        activity: '/images/star-3.png'
+    };
+
+    function drawMarker(type, coords) {
+        var latLng = new google.maps.LatLng(coords[0], coords[1]);
+        var iconURL = iconURLs[type];
+        var marker = new google.maps.Marker({
+            icon: iconURL,
+            position: latLng
+        });
+        marker.setMap(currentMap);
+        return marker;
+    }
+
+    function removeMarker(type, coords) {
+        // var latLng = new google.maps.LatLng(coords[0], coords[1]);
+        // var iconURL = iconURLs[type];
+        // var marker = new google.maps.Marker({
+        //     icon: iconURL,
+        //     position: latLng
+        // });
+
+
+        marker.setMap(null);
+    }
+
+    $('.hotel-button').on('click', function clickFunc() {
+        var hotelDOM = $('#hotel-choices').val();
+        $('.hotel-selections').append('<div class="itinerary-item"> <span class="title">' + hotelDOM + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
+
+        var ourHotel;
+        hotels.forEach(function(hotel) {
+            if (hotel.name === hotelDOM) {
+                ourHotel = hotel;
+            }
+        });
+        var ourPlace = places.filter(function(place) {
+            return place.id === ourHotel.placeId;
+        })[0];
+
+        var hotelMarker = drawMarker('hotel', ourPlace.location);
+
+        $('.hotel-button').off('click');
+
+        $('.remove').on('click', function() {
+            $('.itinerary-item').remove();
+            $('.hotel-button').on('click', clickFunc);
+            hotelMarker.setMap(null);
+        });
+    });
+
+
+    $('.restaurant-button').on('click', function() {
+        var restaurantDOM = $('#restaurant-choices').val();
+        var restaurantDOMnoSpaces = restaurantDOM.split(' ').join('').replace("'", '');
+
+        $('.restautant-selections').append(`<div class="itinerary-item ${restaurantDOMnoSpaces}"> <span class="title">` + restaurantDOM + `</span><button class="btn btn-xs btn-danger remove btn-circle btn-${restaurantDOMnoSpaces}">x</button></div>`);
+        var ourRestaurant;
+        restaurants.forEach(function(restaurant) {
+            if (restaurant.name === restaurantDOM) {
+                ourRestaurant = restaurant;
+            }
+        });
+
+        var ourPlace = places.filter(function(place) {
+            return place.id === ourRestaurant.placeId;
+        })[0];
+
+        var restaurantMarker = drawMarker('restaurant', ourPlace.location);
+
+        $('.btn-' + restaurantDOMnoSpaces).on('click', function() {
+            $('.' + restaurantDOMnoSpaces).remove();
+            restaurantMarker.setMap(null);
+        });
+
+    });
+
+
+    $('.activity-button').on('click', function() {
+
+        var activityDOM = $('#activity-choices').val();
+        var activityDOMnoSpaces = activityDOM.split(' ').join('').replace("'", '');
+
+
+        $('.activity-selections').append(`<div class="itinerary-item ${activityDOMnoSpaces}"> <span class="title">` + activityDOM + `</span><button class="btn btn-xs btn-danger remove btn-circle btn-${activityDOMnoSpaces}">x</button></div>`);
+
+        var ourActivity;
+        activities.forEach(function(activity) {
+            if (activity.name === activityDOM) {
+                ourActivity = activity;
+            }
+        });
+        var ourPlace = places.filter(function(place) {
+            return place.id === ourActivity.placeId;
+        })[0];
+
+        var activityMarker = drawMarker('activity', ourPlace.location);
+
+        $('.btn-' + activityDOMnoSpaces).on('click', function() {
+            $('.' + activityDOMnoSpaces).remove();
+            activityMarker.setMap(null);
+        });
+    });
+
+    var num = 1;
+    $('#day-add').on('click', function() {
+
+        $('#day-add').before('<button class="btn btn-circle day-btn">' + num + '</button>');
+        num++;
+    });
 
 });
--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,21 @@ var models = require('../models');
 var Hotel = require('../models/hotel');
 var Restaurant = require('../models/restaurant');
 var Activity = require('../models/activity');
+var Place = require('../models/place');
 
 router.get('/', function (req, res, next) {
   Promise.all([
     Hotel.findAll(),
     Restaurant.findAll(),
-    Activity.findAll()
+    Activity.findAll(),
+    Place.findAll()
   ])
-  .spread(function (hotels, restaurants, activities) {
+  .spread(function (hotels, restaurants, activities, places) {
     res.render('index', {
       hotels: hotels,
       restaurants: restaurants,
-      activities: activities
+      activities: activities,
+      places: places
     });
   })
   .catch(next);
